297a5575ee26b725e25c778094cebb95c69949c3
DESKTOP-JJHHJ8M\admin <victoryline629@naver.com>
2023-01-13 16:57:59 +0900

    프로젝트 생성

---
+++ src/main/java/com/fproject/project_team3/ProjectTeam3Application.java
@@ -0,0 +1,13 @@
+package com.fproject.project_team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTeam3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTeam3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fproject/project_team3/ProjectTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fproject.project_team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTeam3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
