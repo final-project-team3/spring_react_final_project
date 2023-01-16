f96686f6f2c99192ee6b94a74e49bc4c7e092296
DESKTOP-JJHHJ8M\admin <victoryline629@naver.com>
2023-01-16 09:41:09 +0900

    프로젝트 기본 틀

---
+++ src/main/java/com/fproject/project_team3/mapper/sellerMapper.java
@@ -0,0 +1,27 @@
+package com.fproject.project_team3.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class sellerMapper {
+
+    //    HSH
+
+    //    HSH
+
+    //    LYS
+
+    //    LYS
+
+    //    GJY
+
+    //    GJY
+
+    //    BJH
+
+    //    BJH
+
+    //    LSH
+
+    //    LSH
+}
