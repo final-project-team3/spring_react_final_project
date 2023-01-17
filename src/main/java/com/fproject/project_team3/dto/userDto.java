cd01c8e09ac30eee149b9700165f2f38baf563ee
dbstjd608 <dbstjd608@gmail.com>
2023-01-17 10:22:42 +0900

    dsadad

--- src/main/java/com/fproject/project_team3/dto/userDto.java
+++ src/main/java/com/fproject/project_team3/dto/userDto.java
@@ -9,7 +9,16 @@ public class userDto {
     //    HSH
 
     //    LYS
-
+    private int userInfoNum;
+    private String userId;
+    private String userPass;
+    private String userTel;
+    private String userBirth;
+    private String userGender;
+    private String userAddrNum;
+    private String userAddrJibun;
+    private String userAddrRoad;
+    private String userAddrDetail;
     //    LYS
 
     //    GJY
