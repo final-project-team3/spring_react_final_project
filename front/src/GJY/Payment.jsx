f2eae64a840b816ea1172e434ad174e81f69ec48
DESKTOP-6M69AE9\admin <nmk02@naver.com>
2023-01-17 10:26:16 +0900

    commit

--- front/src/GJY/Payment.jsx
+++ front/src/GJY/Payment.jsx
@@ -37,7 +37,7 @@ const styles = {
   vertical: {
     borderLeft: 'solid lightgray',
     borderWidth: 0.5,
-    height: 400,
+    height: 260,
     position:'absolute',
     left: '50%',
   },
@@ -61,22 +61,15 @@ const FormBlockBody = styled.div`
   text-align: left;
 `;
 
-const Input = styled.input`
-  font-size: 14px;
-  height: 48px;
-  background: #fff;
-  line-height: 16px;
-  border: 1px solid #acacac;
-  width: 100%;
-  box-sizing: border-box;
-  padding: 2px 8px;
-  border-radius: 2px;
-  appearance: none;
-`;
 
-  function Payment(props) {
-    const [order, setOrder] = useState(0);
-    const [delivery, setDelivery] = useState(3000);
+
+
+function Payment(props) {
+  const [order, setOrder] = useState(0);
+  const [delivery, setDelivery] = useState(3000);
+
+
+
 
     useEffect(() => {
       if (order >= 30000) {
@@ -93,98 +86,102 @@ const Input = styled.input`
 
     const ChangeOpt = () => {
       setOrder(40000);
-      if (order >= 30000) {
-        setDelivery(0);
-      } else if (order < 30000){
-        setDelivery(3000);
-      }
     }
-  return (
-    <div className={'container'}>
-
-      <h1 className={'mt-4 text-center'}>결제 페이지</h1>
-      <h3 className={'text-start mt-5'}>회원 주문 정보</h3>
-      <hr/>
-      <div className={'col-6'}>
-        <label htmlFor={'name'}>이름</label>
-        <input type="text" id={'name'} className={'form-control mt-2'} placeholder={'이름을 입력해주세요.'}/>
-      </div>
-      <div className={'col-6'}>
-        <label htmlFor={'phone'} className={'mt-3'}>휴대폰</label>
-        <input type="tel" id={'phone'} className={'form-control mt-2'} placeholder={'휴대폰번호를 입력해주세요. 하이픈(-) 제외'}/>
-      </div>
-      <div className={'col-6'}>
-        <FormBlock>
-          <FormBlockHead>
-            <AsteriskRed>*</AsteriskRed> 주소
-          </FormBlockHead>
-          <FormBlockBody>
-            <Popup/>
-            <Input className={'my-1'} id={"sigunguCode"} placeholder={'우편번호'} readOnly={true}/>
-            <Input className={'my-1'} id={"jibunAddress"} placeholder={'지번 주소'} readOnly={true}/>
-            <Input className={'my-1'} id={"roadAddress"} placeholder={'도로명 주소'} readOnly={true}/>
-            <Input className={'my-1'} id={"addressDetail"} placeholder={'상세주소를 입력해주세요.'}/>
-          </FormBlockBody>
-        </FormBlock>
-      </div>
-      <div className={'col-6'}>
-        <label htmlFor={'request'} className={'mt-3'}>배송 메모</label>
-        <select name={'request'} id={'request'} className={'form-select mt-2'}>
-          <option value="request-1">배송 메모를 선택해 주세요.</option>
-          <option value="request-2">요청사항을 직접 입력합니다.</option>
-          <option value="request-3">배송 전에 미리 연락 바랍니다.</option>
-          <option value="request-4">부재시 경비실에 맡겨 주세요.</option>
-        </select>
-      </div>
-      <br/>
-      <hr/>
-      <br/>
-      <div className={'col-12'}>
-        <div className={'row'}>
-          <img src="/java505/intellij/team3_project/spring_react_final_project/front/public/logo192.png" alt="제품 사진" className={'col-3'} />
-          <div className={'col-3 ms-5'}>
-            <div className={'row'}>
-              <label className={'float-start col-3'} htmlFor={'productName'}>제품명</label>
-              <p id={'productName'} className={'col-3'}>{productName}</p>
+    return (
+      <div className={'container'}>
+        <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"></script>
+        <h1 className={'mt-4 text-center'}>결제 페이지</h1>
+        <h3 className={'text-start mt-5'}>회원 주문 정보</h3>
+        <hr/>
+        <div className={'col-6'}>
+          <label htmlFor={'name'}>이름</label>
+          <input type="text" id={'name'} className={'form-control mt-2'} placeholder={'이름을 입력해주세요.'}/>
+        </div>
+        <div className={'col-6'}>
+          <label htmlFor={'phone'} className={'mt-3'}>휴대폰</label>
+          <input type="tel" id={'phone'} className={'form-control mt-2'} placeholder={'휴대폰번호를 입력해주세요. 하이픈(-) 제외'}/>
+        </div>
+        <div className={'col-6'}>
+          <FormBlock>
+            <FormBlockHead>
+              <AsteriskRed>*</AsteriskRed> 주소
+            </FormBlockHead>
+            <FormBlockBody>
+              <Popup/>
+              <div className={'row ms-1'}>
+                <input className={'my-1'} id={"sigunguCode"} placeholder={'우편번호'} readOnly={true}/>
+              </div>
+              <div className={'row ms-1'}>
+                <input className={'my-1'} id={"jibunAddress"} placeholder={'지번 주소'} readOnly={true}/>
+              </div>
+              <div className={'row ms-1'}>
+                <input className={'my-1'} id={"roadAddress"} placeholder={'도로명 주소'} readOnly={true}/>
+              </div>
+              <div className={'row ms-1'}>
+                <input className={'my-1'} id={"addressDetail"} placeholder={'상세주소를 입력해주세요.'}/>
+              </div>
+            </FormBlockBody>
+          </FormBlock>
+        </div>
+        <div className={'col-6'}>
+          <label htmlFor={'request'} className={'mt-3'}>배송 메모</label>
+          <select name={'request'} id={'request'} className={'form-select mt-2'}>
+            <option value="request-1">배송 메모를 선택해 주세요.</option>
+            <option value="request-2">요청사항을 직접 입력합니다.</option>
+            <option value="request-3">배송 전에 미리 연락 바랍니다.</option>
+            <option value="request-4">부재시 경비실에 맡겨 주세요.</option>
+          </select>
+        </div>
+        <br/>
+        <hr/>
+        <br/>
+        <div className={'col-12'}>
+          <div className={'row'}>
+            <img src="/logo192.png" alt="제품 사진" className={'col-3'}/>
+            <div className={'col-3 ms-5'}>
+              <div className={'row'}>
+                <label className={'float-start col-3'} htmlFor={'productName'}>제품명</label>
+                <p id={'productName'} className={'col-3'}>{productName}</p>
+              </div>
+              <div className={'row'}>
+                <label className={'float-start col-3'} htmlFor={'productOpt'}>옵션</label>
+                <p id={'productOpt'} className={'col-3'}>{productOpt}</p>
+              </div>
+              <div className={'row'}>
+                <label className={'float-start col-3'} htmlFor={'productCnt'}>수량</label>
+                <p id={'productCnt'} className={'col-3'}>{productCnt}</p>
+              </div>
+              <button className={'btn btn-warning'} onClick={ChangeOpt}>옵션 변경</button>
             </div>
-            <div className={'row'}>
-              <label className={'float-start col-3'} htmlFor={'productOpt'}>옵션</label>
-              <p id={'productOpt'} className={'col-3'}>{productOpt}</p>
+            <div className={'col-6'} style={styles.vertical}>
+              <div className={'row'}>
+                <label className={'float-start col-3'} htmlFor={'order'}>주문금액</label>
+                <p id={'order'} className={'col-3'}>{order} 원</p>
+              </div>
+              <div className={'row'}>
+                <label className={'float-start col-3'} htmlFor={'delivery'}>배송비</label>
+                <p id={'delivery'} className={'col-3'}>{delivery} 원</p>
+              </div>
+              <div className={'row'}>
+                <label htmlFor={"discount"} className={'float-start col-3'}>할인 적용</label>
+                <p id={'discount'} className={'col-3'}>{discount} 원</p>
+                <button className={'btn btn-success col-2'}>포인트/쿠폰</button>
+              </div>
+              <div className={'row'}>
+                <label className={'float-start col-3'} htmlFor={'cost'}>결제 금액</label>
+                <p id={'cost'} className={'col-3'}>{cost} 원</p>
+              </div>
             </div>
-            <div className={'row'}>
-              <label className={'float-start col-3'} htmlFor={'productCnt'}>수량</label>
-              <p id={'productCnt'} className={'col-3'}>{productCnt}</p>
-            </div>
-            <button className={'btn btn-warning'} onClick={ChangeOpt}>옵션 변경</button>
           </div>
-          <div className={'col-6'} style={styles.vertical}>
-            <div className={'row'}>
-              <label className={'float-start col-3'} htmlFor={'order'}>주문금액</label>
-              <p id={'order'} className={'col-3'}>{order} 원</p>
-            </div>
-            <div className={'row'}>
-              <label className={'float-start col-3'} htmlFor={'delivery'}>배송비</label>
-              <p id={'delivery'} className={'col-3'}>{delivery} 원</p>
-            </div>
-            <div className={'row'}>
-              <label htmlFor={"discount"} className={'float-start col-3'}>할인 적용</label>
-              <p id={'discount'} className={'col-3'}>{discount} 원</p>
-              <button className={'btn btn-success col-2'}>포인트/쿠폰</button>
-            </div>
-            <div className={'row'}>
-              <label className={'float-start col-3'} htmlFor={'cost'}>결제 금액</label>
-              <p id={'cost'} className={'col-3'}>{cost} 원</p>
-            </div>
+          <div className={'d-flex justify-content-end me-5'}>
+            <button className={'btn btn-info me-5 fs-3'}>결제하기</button>
+            <button className={'btn btn-dark fs-3'}>취소</button>
           </div>
         </div>
-
+        <br/>
+        <button onClick={onClickPayment}>결제하기</button>
       </div>
+    );
 
-
-
-
-    </div>
-  );
 }
-
 export default Payment;
