6d0201ed031746273ef42287e9b89ca0d89e12e7
dbstjd608 <dbstjd608@gmail.com>
2023-01-18 10:43:25 +0900

    ㅎㄹㅇㄶㅇㄹ

--- front/src/LYS/Review.jsx
+++ front/src/LYS/Review.jsx
@@ -5,7 +5,14 @@ import {
     Link, Switch, Route, BrowserRouter
 } from 'react-router-dom';
 
+const ReviewText = styled.textarea`
+    width: 50%;
+    height: 6.25em;
+    resize: none;
+`;
+
 function Review() {
+
     return (
         <div className="mt-5 container">
             <h1 className={"text-start"}>제품 리뷰</h1>
@@ -13,29 +20,29 @@ function Review() {
                 <ul>
                     <li>
                         <article>
-                            <div className={"review_info"}>
-                                <div className={"reviewer_id"}></div>
-                                <div className={"star_point"}></div>
-                                <div className={"seller_id"}></div>
-                                <div className={"registration_date"}></div>
+                            <div className="text-md-start">
+                                <div className={"review_info"}>
+                                    <div className={"reviewer_id"}></div>
+                                    <div className={"star_point"}></div>
+                                    <div className={"seller_id"}></div>
+                                    <div className={"registration_date"}></div>
+                                </div>
                             </div>
-
                             {/*<div className={"review_img"}></div>*/}
-
-                            <div className={"review_content"}>
-                                <div className={'mt-3 border border-dark'}>
-                                    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci aliquid
-                                        assumenda commodi culpa, eos esse est eum excepturi expedita ipsa laudantium
-                                        natus numquam praesentium quibusdam, quisquam repellat totam voluptatem!</h4>
+                            <div className={"text-md-start"}>
+                                <div className={"review_content"}>
+                                    <ReviewText disabled={true}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi, assumenda at autem blanditiis consectetur </ReviewText>
                                 </div>
                             </div>
 
-                            <div className={"review_useful"}>
-                                <div>이 상품평이 도움이 되었나요?</div>
-                                <button>네</button>
-                                &nbsp;&nbsp;&nbsp;&nbsp;
-                                <button className={"btn"}>아니요</button>
-                                <p>총 {}명의 회원이 도움이 되었습니다.</p>
+                            <div className={"text-md-start"}>
+                                <div className={"review_useful"}>
+                                    <p>이 상품평이 도움이 되었나요?</p>
+                                    <button className={"btn btn-primary"}>네</button>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <button className={"btn btn-primary"}>아니요</button>
+                                    <p className={"my-2"}>총 {}명의 회원이 도움이 되었습니다.</p>
+                                </div>
                             </div>
                         </article>
                     </li>
@@ -44,6 +51,7 @@ function Review() {
         </div>
     );
 }
+
 
 export default Review;
 
