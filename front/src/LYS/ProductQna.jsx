97a0f8197ce51f1c160e67ac1dbbd1ff3f2a609c
dbstjd608 <dbstjd608@gmail.com>
2023-02-07 13:52:27 +0900

    ㅁㄴㅇㅁㄴㅇㅁㄴㅇ

--- front/src/LYS/ProductQna.jsx
+++ front/src/LYS/ProductQna.jsx
@@ -1,7 +1,23 @@
 import React, {useEffect, useState} from "react";
 import './ProductQna.css';
+import axios from "axios";
+import {useParams} from "react-router-dom";
 
 function ProductQna(props) {
+    const {productNum} = useParams();
+    const [productInfo, setProductInfo] = useState();
+
+    useEffect(() => {
+        return async () => {
+            const {data} = await axios.get("http://localhost:8080/getProductInfoFromDetail", {
+                params: {
+                    productNum: productNum,
+                }
+            })
+            setProductInfo(data.productInfo);
+        }
+    }, [])
+
     return (
         <li className="product-qna tab-contents__content">
             <div id="prod-inquiry-list" className="prod-tab">
@@ -16,9 +32,11 @@ function ProductQna(props) {
                             <em className="prod-inquiry-item__label">질문</em>
                             <div className="prod-inquiry-item__wrap">
                                 <strong className="prod-inquiry-item__author"></strong>
-                                <div className="prod-inquiry-item__selected-option">그래파이트 | 코어i5 11세대 | 256GB | 16GB
-                                    | WIN11 Home | 8PT-00030&nbsp;&nbsp;주식회사 스터프
+                                <div
+                                    className="prod-inquiry-item__selected-option">{`${productInfo?.productName}`}
+                                    &nbsp;&nbsp;{`${productInfo?.productSellerBusinessName}`}
                                 </div>
+                                <div className="prod-inquiry-item__userId">{props.userId}</div>
                                 <div className="prod-inquiry-item__content">{props.qnaContent}
                                 </div>
                                 <div className="prod-inquiry-item__time">{props.qnaRegistrationDate}</div>
@@ -26,11 +44,11 @@ function ProductQna(props) {
 
                             <div className="prod-inquiry-item__reply">
                                 <i className="prod_inquiry-item__reply__icon"></i>
-                                {/*<em className="prod-inquiry-item__reply__label">답변</em>*/}
                                 <div className="prod-inquiry-item__reply__wrap">
-                                    <div className={"row"} style={{margin:0, padding:0}}>
-                                        <strong className="prod-inquiry-item__reply__author col-6">주식회사 스터프</strong>
-                                        <p className={"col-6"} style={{alignItems: "end", width: 70}}>답변완료</p>
+                                    <div className={"row"}>
+                                        <strong className="prod-inquiry-item__reply__author col-6">{`${productInfo?.productSellerBusinessName}`}</strong>
+                                        <p className={"col-6"}
+                                           style={{alignItems: "end", margin: 0, padding: 0}}>{props.qnaState}</p>
                                     </div>
                                     <div className="prod-inquiry-item__reply__content">{props.qnaAnswer}</div>
                                     <div
