edc402105271bdec2fd7ea77f36fbf37cc6f79b1
DESKTOP-6M69AE9\admin <nmk02@naver.com>
2023-02-13 19:23:31 +0900

    집가기전commit - 통계ing

--- front/src/BJH/SellerMyPage.js
+++ front/src/BJH/SellerMyPage.js
@@ -76,18 +76,18 @@ function SellerMyPage(props) {
                 </div>
                 <div className={"row"}>
                     <div className="border-end border-opacity-10 listContainer col-6">
-                        <a href="#" className="item">
-                            <div className="ps-5 icon"><img style={{
-                                width: 24,
-                                height: 24
-                            }} src={"./Img/Bjh/delivery.png"}/>
-                            </div>
-                            <div className="text">발송한 상품 내역(삭제예정)</div>
-                            <div className="pe-5 right"><img style={{
-                                width: 24,
-                                height: 24
-                            }} src={"./Img/Bjh/right.png"}/></div>
-                        </a>
+                        {/*<a href="#" className="item">*/}
+                        {/*    <div className="ps-5 icon"><img style={{*/}
+                        {/*        width: 24,*/}
+                        {/*        height: 24*/}
+                        {/*    }} src={"./Img/Bjh/delivery.png"}/>*/}
+                        {/*    </div>*/}
+                        {/*    /!*<div className="text">발송한 상품 내역(삭제예정)</div>*!/*/}
+                        {/*    <div className="pe-5 right"><img style={{*/}
+                        {/*        width: 24,*/}
+                        {/*        height: 24*/}
+                        {/*    }} src={"./Img/Bjh/right.png"}/></div>*/}
+                        {/*</a>*/}
                         <a href="/productR2" className="item">
                             <div className="ps-5 icon"><img style={{
                                 width: 24,
@@ -139,21 +139,21 @@ function SellerMyPage(props) {
                                 height: 24
                             }} src={"./Img/Bjh/right.png"}/></div>
                         </a>
-                        <a href="/orderUserList" className="item">
-                            <div className="ps-5 icon"><img style={{
-                                width: 24,
-                                height: 24
-                            }} src={"./Img/Bjh/wallet.png"}/>
-                            </div>
-                            <div className="text">
-                                <span>주문한 고객 목록(삭제 예정)</span>
-                            </div>
-                            <div className="pe-5 right"><img style={{
-                                width: 24,
-                                height: 24
-                            }} src={"./Img/Bjh/right.png"}/>
-                            </div>
-                        </a>
+                        {/*<a href="/orderUserList" className="item">*/}
+                        {/*    <div className="ps-5 icon"><img style={{*/}
+                        {/*        width: 24,*/}
+                        {/*        height: 24*/}
+                        {/*    }} src={"./Img/Bjh/wallet.png"}/>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="text">*/}
+                        {/*        <span>주문한 고객 목록(삭제 예정)</span>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="pe-5 right"><img style={{*/}
+                        {/*        width: 24,*/}
+                        {/*        height: 24*/}
+                        {/*    }} src={"./Img/Bjh/right.png"}/>*/}
+                        {/*    </div>*/}
+                        {/*</a>*/}
                         <a href="/payUserList" className="item">
                             <div className="ps-5 icon"><img style={{
                                 width: 24,
