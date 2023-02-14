676368a7b851e024e697cd8e02735ac875b85607
dbstjd608 <dbstjd608@gmail.com>
2023-02-14 10:26:58 +0900

    존재하지 않는 페이지

--- front/src/LYS/NotExist.jsx
+++ front/src/LYS/NotExist.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-function NotExist(){
-    return(
-        <h1>존재하지 않는 페이지 입니다</h1>
+function NotExist() {
+    return (
+        <div>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <h1 className={"text-center"}>존재하지 않는 페이지 입니다</h1>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        </div>
     )
 }
+
 export default NotExist;
