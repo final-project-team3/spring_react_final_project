b8d6f904c9c16c0fe0555b9446a6d70f6483ee0d
DESKTOP-II3FO3L\nmk02 <nmk01228@gmail.com>
2023-03-16 19:35:40 +0900

    firebase기간만료-업데이트(~04/15)

--- front/src/firebase.js
+++ front/src/firebase.js
@@ -8,14 +8,14 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAqi9fvwTwQG3oAGsnBDyddSYqLATnKxKQ",
-  authDomain: "react-20f81.firebaseapp.com",
-  databaseURL: "https://react-20f81-default-rtdb.firebaseio.com",
-  projectId: "react-20f81",
-  storageBucket: "react-20f81.appspot.com",
-  messagingSenderId: "526022452806",
-  appId: "1:526022452806:web:5af6f1ba4d15e93e8f8c47",
-  measurementId: "G-P5P45Z13BF"
+  apiKey: "AIzaSyANIHc_-M7KIEA53NArakODmsHKC6nxWc0",
+  authDomain: "react2-b17ac.firebaseapp.com",
+  databaseURL: "https://react2-b17ac-default-rtdb.firebaseio.com/",
+  projectId: "react2-b17ac",
+  storageBucket: "react2-b17ac.appspot.com",
+  messagingSenderId: "778938266602",
+  appId: "1:778938266602:web:5c84c8e621613f193df893",
+  // measurementId: "G-P5P45Z13BF"
 };
 
 // Initialize Firebase
